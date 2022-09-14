785c71f9fea9b5d709b572e8fec62126f1db225e
konstantin bondarenko <zois1990@gmail.com>
2022-09-14 17:05:24 +0200

    method CreateArrayNoMoreThreeChar upgraded

--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
 ﻿string[] CreateArrayNoMoreThreeChar(string[] firstArray)
 {
     int countString = 0;
-    for (int i = 0; i < firstArray.Length; i++)
-        if (firstArray[i].Length < 4)
-            countString++;
-    string[] resultArray = new string[countString];
+    int[] apprIndex = new int[firstArray.Length];
     for (int i = 0, j = 0; i < firstArray.Length; i++)
         if (firstArray[i].Length < 4)
         {
-            resultArray[j] = firstArray[i];
+            countString++;
+            apprIndex[j] = i;
             j++;
         }
+    string[] resultArray = new string[countString];
+    if (countString > 0)
+    {
+        resultArray[0] = firstArray[apprIndex[0]];
+        for (int i = 1; i < firstArray.Length; i++)
+        {
+            if (apprIndex[i] > 0)
+                resultArray[i] = firstArray[apprIndex[i]];
+            else break;
+        }
+    }
     return resultArray;
 }
-string[] firstArray = {"12", "34", "asfasf", "sf", "sfa"};
+string[] firstArray = {"12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa"};
 string[] aaa = CreateArrayNoMoreThreeChar(firstArray);
 for (int i = 0; i < aaa.Length; i++)
     Console.Write(aaa[i] + " ");
