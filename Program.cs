cba57a3d5a52152c9298a84b31c94096d283e198
konstantin bondarenko <zois1990@gmail.com>
2022-09-15 11:32:13 +0200

    nethod string to array complete

--- Program.cs
+++ Program.cs
@@ -25,37 +25,25 @@
 
 string[] StringToArray(string str, char separator)
 {
-    int countString = 0;
-    int[] apprIndex = new int[str.Length];
-    for (int i = 0, j = 0; i < str.Length; i++)
+    int countString = 1;
+    for (int i = 0; i < str.Length; i++)
         if (str[i] == separator)
-        {
             countString++;
-            apprIndex[j] = i;
+    string[] result = new string[countString];
+    for (int i = 0, j = 0; i < str.Length; i++)
+        if (str[i] != separator)
+            result[j] += str[i];
+        else
+        {
+            i++;
             j++;
         }
-    string[] resultArray = new string[countString + 1];
-    if (countString > 0)
-    {
-        if (apprIndex[0] > 0)
-        {
-            for (int i = 0; i < apprIndex[0]; i++)
-                resultArray[0] += str[i];
-        }
-        else resultArray[0] = str;
-        for (int i = 1; i < str.Length; i++)
-            if (apprIndex[i] > 0)
-                for (int j = apprIndex[i - 1] + 2; j < apprIndex[i]; j++)
-                    resultArray[i] += str[j];
-            else break;
-    }
-    return resultArray;
+    return result;
 }
 
-//string[] firstArray = {"12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa", "12", "34", "asfasf", "sf", "sfa"};
-//string[] aaa = CreateArrayNoMoreThreeChar(firstArray);
-//for (int i = 0; i < aaa.Length; i++)
-//    Console.Write(aaa[i] + " ");
 string[] str = StringToArray(Console.ReadLine(), Convert.ToChar(Console.ReadLine()));
 for (int i = 0; i < str.Length; i++)
     Console.WriteLine(str[i] + " ");
+string[] aaa = CreateArrayNoMoreThreeChar(str);
+for (int i = 0; i < aaa.Length; i++)
+    Console.Write(aaa[i] + " ");
